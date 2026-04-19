3d34673efff74ca60bbcdc125c292f73314efb94
Kisan Carts <supportkisancarts24@gmail.com>
2026-04-19 19:34:32 +0530

    fix build config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -22,9 +22,9 @@ const eslintConfig = [
   },
   {
     rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-      "react-hooks/exhaustive-deps": "off",
-      "@next/next/no-img-element": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      "@next/next/no-img-element": "warn",
     },
   },
 ];
